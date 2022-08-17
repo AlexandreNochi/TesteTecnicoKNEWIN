bee79e1e1af445c77c3c817464d84965f429dc38
Alexandre Nochi <alexandrenochi@outlook.com>
2022-08-17 20:22:58 -0300

    Adicionadas as validações nas models

--- TesteTecnicoKNEWIN/Models/Post.cs
+++ TesteTecnicoKNEWIN/Models/Post.cs
@@ -1,11 +1,31 @@
-﻿namespace TesteTecnicoKNEWIN.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TesteTecnicoKNEWIN.Models
 {
     public class Post
     {
-        int Id;
-        int Author;
-        // PublicationDate;
-        string Title;
-        string Content;
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int IdAuthor { get; set; }
+
+        public DateTime PublicationDate { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Content { get; set; }
+
+        public Post(int id, int idAuthor, string title, string content)
+        {
+            Id = id;
+            IdAuthor = idAuthor;
+            PublicationDate = DateTime.Now;
+            Title = title;
+            Content = content;
+        }
     }
 }
--- TesteTecnicoKNEWIN/Models/User.cs
+++ TesteTecnicoKNEWIN/Models/User.cs
@@ -1,9 +1,26 @@
-﻿namespace TesteTecnicoKNEWIN.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Xml.Linq;
+
+namespace TesteTecnicoKNEWIN.Models
 {
     public class User
     {
-        int Id;
-        string Name;
-        string Email;
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required(AllowEmptyStrings = false)]
+        public string Name { get; set; }
+        
+        [Required(AllowEmptyStrings = false)]
+        public string Email { get; set; }
+
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
     }
 }
